343cf6f57ba4c1261fd5316379a2fff5c04c2fcf
pc <mustafayaylaci4269@gmail.com>
2021-10-19 00:32:36 +0300

    18. ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarDun= 9.20
+
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=11
+
+console.log(euroDun)
+
+let emlakKredileri=["Komut Kredesi","Emlak Komut Kredesi","Kamu Komut Kredisi"]
+
+console.log("<ul>")
+for(let i=0;i<emlakKredileri.length;i++){
+
+    console.log("<li>"+emlakKredileri[i]+"</li>")
+} 
+console.log("</ul>")
+console.log(emlakKredileri)
+
+
+
